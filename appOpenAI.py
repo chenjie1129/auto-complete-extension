a55e8b4888a285efad24bb4fd99dae88c5d59ba2
Jerry <chjfang1129@outlook.com>
2025-02-12 19:45:23 +0800

    新增应用程序文件，集成深度学习模型API进行聊天功能

---
+++ appOpenAI.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-pekekxqtsbnqalbderhqvwpgpshbnmxtekmpbkpjbyagoqti", base_url="https://api.siliconflow.cn/v1")
+response = client.chat.completions.create(
+    model='deepseek-ai/DeepSeek-V2.5',
+    messages=[
+        {'role': 'user', 
+        'content': "中国大模型行业2025年将会迎来哪些机遇和挑战"}
+    ],
+    stream=True
+)
+
+for chunk in response:
+    print(chunk.choices[0].delta.content, end='')
+
